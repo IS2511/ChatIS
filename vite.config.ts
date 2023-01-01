c1538bc92f487cb28beb226f38d619ead4eaf6b2
IS2511 <is2511@bk.ru>
2023-01-01 19:46:29 +0300

    build: use solid-cloudflare adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import cloudflare from "solid-start-cloudflare-workers";
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
+  plugins: [solid({ ssr: false, adapter: cloudflare({}) })],
 });
