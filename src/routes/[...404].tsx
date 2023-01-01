5bd3575343070249a1c18d868b95f10947cb58c3
IS2511 <is2511@bk.ru>
2023-01-01 19:03:29 +0300

    feat: initial commit (mostly the template)

---
+++ src/routes/[...404].tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+        Not Found
+      </h1>
+    </main>
+  );
+}
---
+++ src/routes/v3/index.tsx
@@ -0,0 +1,12 @@
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+        Hello world!
+      </h1>
+      <Counter />
+    </main>
+  );
+}
