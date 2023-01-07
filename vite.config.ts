a0117da39da6b6c3ea8504972757c8ae76606441
IS2511 <is2511@bk.ru>
2023-01-07 11:32:04 +0300

    feat(root): add SUID (MUI port) library, disable SSR
    Disabling SSR is a required sacrifice since SUID doesn't work with SSR.
    I hope it does support SSR in the future...

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import cloudflare from "solid-start-cloudflare-pages";
 import solid from "solid-start/vite";
+import suidPlugin from "@suid/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ adapter: cloudflare({}) })],
+  plugins: [
+    suidPlugin(),
+    // TODO: Turn on SSR when SUID adds support for it (upstream moment)
+    solid({ ssr: false, adapter: cloudflare({})}),
+  ],
 });
