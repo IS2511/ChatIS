23d35340ce54315602866e4315aeb2f51e504e6e
IS2511 <is2511@bk.ru>
2023-01-07 11:28:17 +0300

    feat(root): remove navbar

--- src/root.tsx
+++ src/root.tsx
@@ -17,11 +17,6 @@ import {
 import "./root.css";
 
 export default function Root() {
-  const location = useLocation();
-  const active = (path: string) =>
-    path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
   const darkTheme = createTheme({
     palette: {
       mode: "dark",
@@ -47,13 +42,6 @@ export default function Root() {
           <CssBaseline />
           <Suspense>
             <ErrorBoundary>
-              <nav class="bg-sky-800">
-                <ul class="container flex items-center p-3 text-gray-200">
-                  <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-                    <A href="/">Home</A>
-                  </li>
-                </ul>
-              </nav>
               <Routes>
                 <FileRoutes />
               </Routes>
