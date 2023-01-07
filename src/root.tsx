c41bf70cc6a05e016df06272864a16a062631896
IS2511 <is2511@bk.ru>
2023-01-07 11:25:37 +0300

    feat(root): add Roboto font and material icons

--- src/root.tsx
+++ src/root.tsx
@@ -27,6 +27,14 @@ export default function Root() {
         <Title>ChatIS by IS2511</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </Head>
       <Body>
         <Suspense>
